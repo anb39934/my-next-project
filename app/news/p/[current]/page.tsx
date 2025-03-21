7f05f774e4e925466c4883d8629ee9c5f3c5fc00
anb39934 <anb39934@gmail.com>
2025-03-21 12:15:58 +0900

    もとにもどぢた

--- app/news/p/[current]/page.tsx
+++ app/news/p/[current]/page.tsx
@@ -9,7 +9,6 @@ type Props = {
     current: string;
   };
 };
-export const revalidate = 60;
 
 export default async function Page({ params }: Props) {
   const current = parseInt(params.current, 10);
